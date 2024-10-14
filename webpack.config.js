b8d41b823416b11990039d3c03b35f834c9ff113
Brandon Fernandez <54313265+BrandonFernandez1@users.noreply.github.com>
2024-10-14 17:33:22 +0800

    configring!

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     mode: 'development',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
         publicPath: '/top-todo/'
     },
     devtool: "eval-source-map",
