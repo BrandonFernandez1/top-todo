162148c3a94ff76cedade93799c995f7f35c3240
Brandon Fernandez <54313265+BrandonFernandez1@users.noreply.github.com>
2024-10-14 17:19:05 +0800

    Configuring gh pages

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        publicPath: '/top-todo/'
     },
     devtool: "eval-source-map",
     devServer: {
